645b7e05e136ec5864a8ccc9877a02cf3eaac947
Stiina Riihinen <lionfrosties@gmail.com>
2024-04-21 17:58:10 +0300

    Make own changes to App.css and App.tsx

--- demo01/src/App.tsx
+++ demo01/src/App.tsx
@@ -3,34 +3,24 @@ import './App.css';
 
 const App : React.FC = () : React.ReactElement => {
 
-  const [tervehdys, setTervehdys] = useState<string>("");
-  const [nimi, setNimi] = useState<string>("");
-
+  const [tervehdys, setTervehdys] = useState<string>('');
+  const [nimi, setNimi] = useState<string>('');
+  
   const sanoHeippa = () : void => {
-
-    setTervehdys(`Tervehdys, ${nimi}!`);
-
+    setTervehdys(`Hei ${nimi}!`);
   }
 
   return (
     <>
-    
-      <h1>Demo 1: React-perusteita</h1>
-      
-      <h2>"Hello world!"</h2>
-
-      <input type="text" placeholder="Anna nimesi..." onChange={ ( e ) => { setNimi(e.target.value) } } />
-
-      <button onClick={sanoHeippa}>Sano heippa</button>
-
-      {(Boolean(tervehdys)) 
-        ? <div className="tervehdysteksti">
+      {(Boolean(tervehdys))
+        ? <div className='tervehdysteksti'>
             {tervehdys}
-          </div> 
+          </div>
         : null
       }
 
-      
+      <input type='text' className='nappi' placeholder='Kirjoita tähän nimi' onChange={(e) => setNimi(e.target.value)}/>
+      <button onClick={sanoHeippa}>Pii!</button>
     </>
   );
 }
